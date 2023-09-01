edffe44cff87f549f1654f7a25ff56649dce8719
mforgioni <matiasforgioni@hotmail.com>
2023-09-01 12:07:02 -0300

    primer commit

---
+++ javascript/programas.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const iconBars = document.querySelector(".fa-bars");
+const iconTimes = document.querySelector(".fa-times");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+  iconBars.style.display = navMenu.classList.contains("nav-menu_visible")
+    ? "none"
+    : "block";
+  iconTimes.style.display = navMenu.classList.contains("nav-menu_visible")
+    ? "block"
+    : "none";
+});
